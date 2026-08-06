7749beb0465297b4e12e5f59081562b503fec28c
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2026-08-06 18:45:50 +0100

    added js file

---
+++ day-1.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+
+let greeting = "Hello, World!";
+console.log(greeting);
+
+alert("Hello, World!");
+
+document.body.innerHTML = "Hello, World!";
+
+let name = "Farida";
+console.log("Hello, " + name + "!");
+

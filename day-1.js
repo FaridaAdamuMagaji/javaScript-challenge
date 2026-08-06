6352908d616caaf1d31bec36a029933c4e057d13
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2026-08-06 19:10:53 +0100

    added map method

--- day-1.js
+++ day-1.js
@@ -10,3 +10,32 @@ document.body.innerHTML = "Hello, World!";
 let name = "Farida";
 console.log("Hello, " + name + "!");
 
+function greet () {
+    console.log("Hello, World!");
+}
+greet();
+
+let greets = function () {
+    console.log("Hi, dear!");
+}
+greets();
+
+function add (a,b) {
+    return a + b;
+}
+let result = add(3, 5);
+console.log(result);
+
+let results = add(4, 10);
+console.log(results);
+
+
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled);
+
+const price = [50, 60, 70, 80, 90];
+const total = price.map(num => num * 2);
+const even = price.filter(num => num > 60);
+console.log(total);
+console.log(even);

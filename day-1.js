622c06dd5ff49a72f8319de6211b513e1682b230
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2026-08-07 17:31:05 +0100

    added navigation links

--- day-1.js
+++ day-1.js
@@ -1,11 +1,11 @@
-console.log("Hello, World!");
+/*console.log("Hello, World!");
 
 let greeting = "Hello, World!";
 console.log(greeting);
 
-alert("Hello, World!");
+alert("Hello, Farida you are doing great! keep it up");
 
-document.body.innerHTML = "Hello, World!";
+// document.body.innerHTML = "Hello, World!";
 
 let name = "Farida";
 console.log("Hello, " + name + "!");
@@ -38,4 +38,21 @@ const price = [50, 60, 70, 80, 90];
 const total = price.map(num => num * 2);
 const even = price.filter(num => num > 60);
 console.log(total);
-console.log(even);
+console.log(even);*/
+
+
+// website
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menuToggle");
+    const closeMenu = document.getElementById("closeMenu");
+    const navMenu = document.getElementById("navMenu");
+    if (menuToggle && navMenu && closeMenu) {
+        menuToggle.addEventListener(click, () => {
+            navMenu.classList.toggle("active");
+        });
+        closeMenu.addEventListener(click, () => {
+            navMenu.classList.remove("active");
+        });
+    }
+});
